84e939b41e1d8fac26a3e51235bf61c338f14d76
Christine <teenahshyro@gmail.com>
2019-02-22 12:23:19 +0300

    add jquery

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+
+      <select class="form-control" id="beverage">
+        <option value="1">Coffee</option>
+        <option value="2">Tea</option>
+        <option value="3">Kombucha</option>
+        <option value="4">Water</option>
+      </select>
