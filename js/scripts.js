0497354f367d740b62ee79c8fb8cee52f85a5954
Christine <teenahshyro@gmail.com>
2019-02-22 17:02:16 +0300

    added details in folders

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,2 @@
-
-      <select class="form-control" id="beverage">
-        <option value="1">Coffee</option>
-        <option value="2">Tea</option>
-        <option value="3">Kombucha</option>
-        <option value="4">Water</option>
-      </select>
+$(document).ready(function(){
+$(#"answers").submit(function(event))
