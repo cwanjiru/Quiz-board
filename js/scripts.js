6326b94db396a0d0787fb6700cb9cfe11fab314e
Christine <teenahshyro@gmail.com>
2019-02-25 10:00:17 +0300

    added content in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
+$(document).ready(function(){
+  $("form#quiz-board").submit(function(event) {
+    event.preventDefault();
+    var one =parseInt($("input:radio[name=test]:checked").val());
+    var two =parseInt($("input:radio[name=test1]:checked").val());
+    var three =parseInt($("input:radio[name=test2]:checked").val());
+    var four =parseInt($("input:radio[name=test3]:checked").val());
+    var five =parseInt($("input:radio[name=test4]:checked").val());
+      var total =(one+two+three+four+five);
+      var percent =((total/50)*100)
+         $("#results").text("you have scored:" +percent+"%");
 
+
+  });
+});
